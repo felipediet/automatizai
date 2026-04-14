0438dc12129e16dafcf9a54dddaca60bf82b3c5e
Felipediet <fefola@gmail.com>
2026-04-14 17:40:14 -0300

    Aprendendo a Ler o Trace do Playwright

--- velo/playwright.config.ts
+++ velo/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     baseURL: 'http://localhost:5173/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
 
     // Tempo máximo para ações (clicks, preenchimentos, etc.)
     // Quando o valor é 0, herda o limite do timeout geral do teste
