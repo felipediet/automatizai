dee35edb84d2f6e5406b57f55b0f68f4cd0d46eb
Felipediet <fefola@gmail.com>
2026-04-14 15:54:51 -0300

    Testando estados de erro e mensagens de feedback e AriaSnapshot

--- velo/playwright/e2e/pedidos.spec.ts
+++ velo/playwright/e2e/pedidos.spec.ts
@@ -20,10 +20,12 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
 
     // Act
         //Checkpoint 2: Preencher o campo de busca com o número do pedido
-        await page.getByTestId('search-order-id').fill(order)
+        //await page.getByTestId('search-order-id').fill(order)
+        await page.getByRole('textbox', { name: 'Número do Pedido'}).fill(order)
 
         //Checkpoint 3: Clicar no botão de buscar pedido
-        await page.getByTestId('search-order-button').click()
+        //await page.getByTestId('search-order-button').click()
+        await page.getByRole('button', { name: 'Buscar Pedido'}).click()
 
     // Assert
         //Checkpoint 4: Verificar se o pedido foi encontrado
@@ -46,5 +48,50 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
 
         // await expect(page.getByText('APROVADO')).toBeVisible();
         // //await expect(page.getByText('APROVADO')).toContainText('APROVADO');
+
+  });
+
+
+
+  test('deve exibir a mensagem quando o pedido nao é encontrado', async ({ page }) => {
+    
+    const order = 'VLO-ABC123'
+
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+    
+    await page.getByRole('textbox', { name: 'Número do Pedido'}).fill(order)
+    await page.getByRole('button', { name: 'Buscar Pedido'}).click() 
+    
+    //Opção 0
+    // await expect(page.locator('#root')).toContainText('Pedido não encontrado');
+    // await expect(page.locator('#root')).toContainText('Verifique o número do pedido e tente novamente');
+
+
+    //Opção 1
+    //const title = page.getByText('Pedido não encontrado' })
+
+    //Opção 2
+    // const title = page.getByRole('heading', { name: 'Pedido não encontrado' })
+    // await expect(title).toBeVisible()
+
+
+
+    //Opção 1
+    //const message = page.getByText('Verifique o número do pedido e tente novamente')
+    
+    //Opção 2
+    // const message = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' })
+    // await expect(message).toBeVisible()
+
+    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+        - img
+        - heading "Pedido não encontrado" [level=3]
+        - paragraph: Verifique o número do pedido e tente novamente
+        `)
+
+        
 
   });
