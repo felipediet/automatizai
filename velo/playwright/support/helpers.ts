e3afdcb29ba655babce21f26efb193d16440764d
Felipediet <fefola@gmail.com>
2026-04-14 16:03:20 -0300

    Add novo metodo generateOrderCode

---
+++ velo/playwright/support/helpers.ts
@@ -0,0 +1,11 @@
+
+
+export function generateOrderCode(): string {
+  const prefix = 'VLO-';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let code = '';
+  for (let i = 0; i < 7; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return prefix + code;
+}
