09990fbe6c2cc9d8ba901d73001215e55281065a
Felipediet <fefola@gmail.com>
2026-04-14 17:24:45 -0300

    Validações de Interface Além do Tradicional com Snapshots

--- velo/playwright/e2e/pedidos.spec.ts
+++ velo/playwright/e2e/pedidos.spec.ts
@@ -68,6 +68,50 @@ test.describe('Consulta de Pedidos', () => {
 
     })
 
+    test('deve consultar um pedido aprovado - com AriaSnapshot', async ({ page }) => {
+
+        //Teste Data
+        const order = 'VLO-KLUYJE'
+
+
+        // Act
+        //await page.getByTestId('search-order-id').fill(order)
+        await page.getByRole('textbox', { name: 'Número do Pedido'}).fill(order)
+
+        //await page.getByTestId('search-order-button').click()
+        await page.getByRole('button', { name: 'Buscar Pedido'}).click()
+ 
+        await expect(page.getByTestId('order-result-VLO-KLUYJE')).toMatchAriaSnapshot(`
+            - img
+            - paragraph: Pedido
+            - paragraph: VLO-KLUYJE
+            - img
+            - text: APROVADO
+            - img "Velô Sprint"
+            - paragraph: Modelo
+            - paragraph: Velô Sprint
+            - paragraph: Cor
+            - paragraph: Lunar White
+            - paragraph: Interior
+            - paragraph: cream
+            - paragraph: Rodas
+            - paragraph: sport Wheels
+            - heading "Dados do Cliente" [level=4]
+            - paragraph: Nome
+            - paragraph: Felipe Diet
+            - paragraph: Email
+            - paragraph: diet@velo.dev
+            - paragraph: Loja de Retirada
+            - paragraph
+            - paragraph: Data do Pedido
+            - paragraph: /\\d+\\/\\d+\\/\\d+/
+            - heading "Pagamento" [level=4]
+            - paragraph: À Vista
+            - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+            `);
+
+    })
+
     test('deve exibir a mensagem quando o pedido nao é encontrado', async ({ page }) => {
     
         const order = generateOrderCode();
